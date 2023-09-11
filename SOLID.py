d1b04609bf7d59bfe65205f46a272879111ffb2a
Ivan Genik <IVGenik@rambler.ru>
2023-09-11 15:37:46 +0500

    Add files via upload

--- SOLID.py
+++ SOLID.py
@@ -1,7 +1,17 @@
 '''
 Принципы проектирования SOLID с примерами на Python
 https://habr.com/ru/companies/otus/articles/651753/
-SOLID_Design_Principles
+2020 SOLID_Design_Principles
 https://github.com/amodiahs/SOLID_Design_Principles
 https://github.com/ivgnk/SOLID_Design_Principles
+
+2021 S.O.L.I.D принципы с примерами на Python
+https://gist.github.com/novikov-nsa/0a1797717a531393ecf84d3d926846f2
+
+2023 Принципы SOLID на примерах Python
+https://tproger.ru/articles/principy-solid-python
+2023 ООП в Python: принципы SOLID для начинающих
+https://proglib.io/p/oop-v-python-principy-solid-dlya-nachinayushchih-2023-07-12
+2023 Принципы SOLID / С Примерами На Python
+https://www.youtube.com/watch?v=WEVGU8qIJyM
 '''
