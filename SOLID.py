cad13a8b3ec3b2cf06ec651848d37395a1f5befe
Ivan Genik <IVGenik@rambler.ru>
2023-09-11 15:17:20 +0500

    Add files via upload

---
+++ SOLID.py
@@ -0,0 +1,7 @@
+'''
+Принципы проектирования SOLID с примерами на Python
+https://habr.com/ru/companies/otus/articles/651753/
+SOLID_Design_Principles
+https://github.com/amodiahs/SOLID_Design_Principles
+https://github.com/ivgnk/SOLID_Design_Principles
+'''
